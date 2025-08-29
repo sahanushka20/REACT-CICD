53350d8b393c532c74cd0d0c660da7f35d417e62
Anushka Sah <sahanushka1820@gmail.com>
2025-08-29 23:26:28 +0530

    Added further logic

--- eslint.config.js
+++ eslint.config.js
@@ -3,10 +3,12 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import react from 'eslint-plugin-react';
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
+   
     files: ['**/*.{js,jsx}'],
     extends: [
       js.configs.recommended,
@@ -15,13 +17,18 @@ export default defineConfig([
     ],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest,  
+      },
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
         sourceType: 'module',
       },
     },
+    plugins: {react, reactHooks, reactRefresh},
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { configDefaults } from 'vitest/config' 
+
 
 // https://vite.dev/config/
 export default defineConfig({
